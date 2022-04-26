b615e08c5802f4db66090a65adbfa09ff191be4a
Odjre <odjre1@gmail.com>
2022-04-26 13:29:13 -0400

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,33 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
+// console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack= 12; 
 
-var fight = function () {
+// console.log(enemyNames.length);
+// console.log(enemyNames[0]);
+// console.log(enemyNames[1]);
+// console.log(enemyNames[2]);
+
+// for(var i=0; i <enemyNames.length; i++) {
+//     console.log(enemyNames[i]);
+//     console.log(i);
+//     console.log(enemyNames[i] + " is at " + i + " index.");
+// }
+
+var fight = function (enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!")
     //ask the player if s/he wants to fight
@@ -43,11 +62,27 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     }
     // if player choses to skip
   } else if (promptFight === "skip" || promptFight === "SKIP") {
-    window.alert(playerName + " has chosen to skip the fight!");
+    //confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes, leave fight
+    if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        // subtract money from playerMoney for skipping
+        playerMoney= playerMoney - 2;
+    }
+    
+    // if no, ask question again by running fight() again
+    else {
+        fight ();
+    }
+
   } else {
     window.alert("You need to choose a valid option. Try again!");
   }
 
 };
 
-fight ();
+for(var i=0; i<enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
